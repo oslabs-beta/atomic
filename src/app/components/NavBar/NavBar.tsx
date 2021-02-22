c58bbf62fa6cf09872dc576983f1d4c009798504
Chandni Patel <chandnip6@gmail.com>
2021-02-21 20:13:01 -0500

    updated navbar buttons

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function NavBar() {
-  return (
-    <div className="NavBar">
-      <button>StateTree3D</button>
-      <button>StateTree3D</button>
-      <button>StateTree3D</button>
-      <button>StateTree3D</button>
-      <button>StateTree3D</button>
-    </div>
-  );
+function NavBar({setTab, tabsList, tab}) {
+  const renderedTabButtons = tabsList.reduce((acc, tabName) => {
+    acc.push(
+      <button
+        className="navBarButtons"
+        key={tabName}
+        style={
+          tab === tabName
+            ? {color: '#E6E6E6', backgroundColor: '#212121'}
+            : {color: '#989898'}
+        }
+        onClick={() => {
+          setTab(tabName);
+        }}>
+        {tabName}
+      </button>,
+    );
+    return acc;
+  }, []);
+  // render the array of NavBar buttons generated above
+  return <div className="NavBar">{renderedTabButtons}</div>;
 }
 
 export default NavBar;
--- src/app/containers/VisualContainer.tsx
+++ src/app/containers/VisualContainer.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from "../components/NavBar/NavBar"
-
+import StateTree from "../components/StateTree3D/StateTree"
 
 function VisualContainer() {
+  const [tab, setTab] = useState<string>('Atom Network');
+
+  const navLists = {
+    "State Tree 3D": (<StateTree/>),
+    "Atom Network": (<p>Atom Network</p>),
+    "Component tree":(<p>Component tree</p>)
+  }
+
+  const tabsList: string[] = Object.keys(navLists);
   return (
     <div className="VisualContainer">
-      <NavBar/>
-    </div>
+    <NavBar setTab={setTab} tabsList={tabsList} tab={tab} />
+    {navLists[tab]}
+  </div>
   );
 }
 
