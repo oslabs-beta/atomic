9dfe223c4ce6d1cab3aa10ff9802c8faf477363f
Chandni Patel <chandnip6@gmail.com>
2021-02-23 19:39:09 -0500

    updated state tree

--- src/app/containers/VisualContainer.tsx
+++ src/app/containers/VisualContainer.tsx
@@ -5,13 +5,13 @@ import AtomNetwork from '../components/AtomNetwork/AtomNetwork';
 import ComponentTree from '../components/ComponentTree/ComponentTree';
 import StateDiff from '../components/StateDiff/StateDiff';
 import StateTree from '../components/StateTree/StateTree';
-import CodeJSONTree from '../components/StateTree/code';
+
 
 function VisualContainer() {
   const [tab, setTab] = useState<string>('State Diff');
 
   const navLists = {
-    'State Diff': <CodeJSONTree />,
+    'State Diff': <StateDiff />,
     'State Tree': <StateTree />,
     'State Tree 3D': <StateTree3D />,
     'Component Tree': <ComponentTree />,
