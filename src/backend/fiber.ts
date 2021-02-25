3dede03593f254998322b6bd8cdee07bf6e90981
Logan <logan.thies@icloud.com>
2021-02-24 19:04:24 -0800

    integrate fiber parser for debug

--- src/backend/fiber.ts
+++ src/backend/fiber.ts
@@ -2,6 +2,8 @@
 /* eslint-disable no-console */
 // const fs = require('fs');
 
+import { buildNodeTree } from './resq';
+
 declare global {
   interface Window {
     __REACT_DEVTOOLS_GLOBAL_HOOK__: any;
@@ -27,7 +29,33 @@ export default (): (() => void) => {
 
     console.log('fiber --> ', fiber);
 
-    return fiber;
+    const testParse = buildNodeTree(fiber);
+
+    console.log('testParse in fiber is--> ', testParse);
+
+    // window.localStorage.setItem('fiber', fiber);
+    // eslint-disable-next-line prettier/prettier
+
+    /* componentTree = {
+      name: root,
+      level: 0,
+      children: [{
+        name: root,
+        level: 1,
+        children: [{}, {}]
+        siblings: [{}, {}]
+      }, {}]
+      siblings: []
+    } */
+
+    // const [{child: [{}, {}]]
+
+    // function helperFunc(node) {
+
+    // }
+    // */
+
+    //traverseG(fiberRoot, helperFunc)
 
     //traverse fiber generate snapshot. {} //reactTime tree generator to get object //reatime getHooks to get atomName useState
     //traverse fiber to get custom ATOMIC HOOK {atoms information} useAtomic(atom, 'name of atom') //useDebuglabel(string) defaults 'atom1, atom2 ... atomX'
