438740b43a1d6a2fab21424de526073b61370b8d
Giovanni Lituma <giovanninet@icloud.com>
2021-02-25 21:08:47 -0500

    Listen messages from backend to send them to content-script

--- src/extension/content-script.ts
+++ src/extension/content-script.ts
@@ -1,10 +1,29 @@
-//Extensions that read or write to web pages utilize a content script. 
-//The content script contains JavaScript that executes in the contexts 
-//of a page that has been loaded into the browser. Content scripts read 
+/* eslint-disable no-console */
+
+//Extensions that read or write to web pages utilize a content script.
+//The content script contains JavaScript that executes in the contexts
+//of a page that has been loaded into the browser. Content scripts read
 //and modify the DOM of web pages the browser visits.
-//Content scripts can communicate with their parent extension 
+//Content scripts can communicate with their parent extension
 //by exchanging messages and storing values using the storage API.
 
 
+window.addEventListener('message', msg => {
+  if (
+    msg.data.source != 'react-devtools-bridge' &&
+    msg.data.source != 'react-devtools-content-script' &&
+    msg.data.source != 'react-devtools-inject-backend'
+  ) {
+    console.log('Window MessageEvent -> ', msg);
+  }
 
-console.log('from content-scripts')
+  const { action }: { action: string } = msg.data;
+  if (action === 'testGetFiber') {
+    console.log('message from backend to background -> ', msg);
+    chrome.runtime.sendMessage(msg.data);
+  }
+});
+
+chrome.runtime.sendMessage({ action: 'injectScript' });
+
+console.log('running content-script.ts');
