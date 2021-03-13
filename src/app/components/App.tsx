d0d50d6e81249132b44a67a15ce40c3e8cbdfa54
Stanley Huang <iamstanleyhuang@gmail.com>
2021-03-12 20:41:07 -0800

    Temporarily render button & snapshotHistory

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -83,68 +83,65 @@ function App(): JSX.Element {
   // runtime.onConnect
 
   //TODO: is useEffect needed??
-  useEffect(() => {
-    const port = chrome.runtime.connect({ name: 'port-from-app-to-bg' });
+  // useEffect(() => {
+  const port = chrome.runtime.connect({ name: 'port-from-app-to-bg' });
 
-    // Port through which messages can be sent and received. The port's onDisconnect event is fired if the extension does not exist.
-    console.log('runtime.Port -> ', port);
+  // Port through which messages can be sent and received. The port's onDisconnect event is fired if the extension does not exist.
+  console.log('runtime.Port -> ', port);
 
-    // INITIALIZE connection to bg script
-    //TODO: update action name to match bg script
-    port.postMessage({
-      action: 'devToolInitialized',
-      tabId: chrome.devtools.inspectedWindow.tabId,
-    });
+  // INITIALIZE connection to bg script
+  //TODO: update action name to match bg script
+  port.postMessage({
+    action: 'devToolInitialized',
+    tabId: chrome.devtools.inspectedWindow.tabId,
+  });
 
-    // listen for messages FROM background script
-    port.onMessage.addListener((message: { action: string; payload: any }) => {
-      console.log('Received message from background script: ', message);
-      let { action, payload } = message;
-      payload = [
-        { filteredSnapshot: prevSnapMock },
-        { filteredSnapshot: curSnapMock },
-      ];
+  // listen for messages FROM background script
+  port.onMessage.addListener((message: { action: string; payload: any }) => {
+    console.log('Received message from background script: ', message);
+    let { action, payload } = message;
 
-      if (action === 'recordSnapshot') {
-        //Set the initial selected useState -> ex: [{name: 'Atom1'}, {name: 'Atom2'}]
-        if (!payload[1] || !filter.length) {
-          //This ensure we only set initially
-          const initialArray: selectedTypes[] = [];
-          //iterate over initial payload received from bg
-          for (let key in payload[0].filteredSnapshot) {
-            // key = atom name
-            initialArray.push({ name: key });
-          }
-          setSelected(initialArray);
-        }
-        // reset snapshot history state with payload
+    if (action === 'RECORD_SNAPSHOT') {
+      //Set the initial selected useState -> ex: [{name: 'Atom1'}, {name: 'Atom2'}]
+      // if (!payload[1] || !filter.length) {
+      //   //This ensure we only set initially
+      //   const initialArray: selectedTypes[] = [];
+      //   //iterate over initial payload received from bg
+      //   for (let key in payload[0].filteredSnapshot) {
+      //     // key = atom name
+      //     initialArray.push({ name: key });
+      //   }
+      //   setSelected(initialArray);
+      // }
+      // reset snapshot history state with payload
+      // payload = array of objects containing atom name keys & node values
+      setSnapshotHistory(payload.atomState);
+      //Set filter Array
+      //TODO: possible refactor if statement
+      // if (payload[1].length === 1 || filter.length === 0)
+      if (!payload[1] || filter.length === 0) {
+        filter = payload;
+        setFilter(payload);
+      } else if (filter.length === 0) {
+        filter.push(payload[0]);
+        setFilter(filter);
+      } else {
+        // push the difference between the objects of atoms with thier nodes
+        const delta: any = diff(
+          payload[payload.length - 2],
+          payload[payload.length - 1]
+        );
+        // filter = array of objects containing differences between snapshots
         // payload = array of objects containing atom name keys & node values
-        setSnapshotHistory(payload);
-        //Set filter Array
-        //TODO: possible refactor if statement
-        if (!payload[1] || filter.length === 0) {
-          filter = payload;
-          setFilter(payload);
-        } else if (filter.length === 0) {
-          filter.push(payload[0]);
+        if (filter.length < payload.length) {
+          filter.push(delta);
           setFilter(filter);
-        } else {
-          // push the difference between the objects of atoms with thier nodes
-          const delta: any = diff(
-            payload[payload.length - 2],
-            payload[payload.length - 1]
-          );
-          // filter = array of objects containing differences between snapshots
-          // payload = array of objects containing atom name keys & node values
-          if (filter.length < payload.length) {
-            filter.push(delta);
-            setFilter(filter);
-          }
         }
       }
-    });
-    console.log('hello from App.tsx');
-  }, []);
+    }
+  });
+  console.log('hello from App.tsx');
+  // }, []);
 
   const renderMainContainer: JSX.Element = (
     <filterContext.Provider value={{ filter, setFilter }}>
@@ -152,14 +149,11 @@ function App(): JSX.Element {
         <snapshotHistoryContext.Provider
           value={{ snapshotHistory, setSnapshotHistory }}
         >
-          <MainContainer />
-          {/* <button
-            onClick={() =>
-              port.postMessage({ action: 'hello from DevTool App' })
-            }
-          >
+          {/* <MainContainer atomState={atomState} /> */}
+          <button onClick={() => port.postMessage({ action: 'GET_SNAPSHOT' })}>
             POST TO Background.ts
-          </button> */}
+          </button>
+          <p>{JSON.stringify(snapshotHistory)}</p>
         </snapshotHistoryContext.Provider>
       </selectedContext.Provider>
     </filterContext.Provider>
