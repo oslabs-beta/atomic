c8d76302ffcc140437af04481529818cd9487512
Stanley Huang <iamstanleyhuang@gmail.com>
2021-03-16 19:59:22 -0700

    Create snapshot context
    Co-authored-by: Chandni Patel <chandnikat@users.noreply.github.com>

--- src/app/containers/SnapShotContainer.tsx
+++ src/app/containers/SnapShotContainer.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { snapshotHistoryContext } from '../components/App';
+import { prevSnapMock } from '../../app/mock/mockStateDiff';
+import Snapshot from '../components/Snapshot/Snapshot';
 
 function SnapShotContainer(): JSX.Element {
+  const { snapshotHistory, setSnapshotHistory } = useContext(
+    snapshotHistoryContext
+  );
+
+  const handleNewData = () => {
+    const copy = { ...prevSnapMock };
+    copy.resetSquaresAtom = { ...copy.resetSquaresAtom };
+    copy.resetSquaresAtom.contents = Math.floor(Math.random() * 10000);
+    setSnapshotHistory(prevState => [...prevState, copy]);
+  };
+
   return (
     <div className="snapShotsContainer">
       <p
@@ -13,8 +27,13 @@ function SnapShotContainer(): JSX.Element {
       >
         ATOMIC{' '}
       </p>
-
+      <button onClick={handleNewData}>ADD SnapShot</button>
       <p>SnapShotContainer</p>
+      {snapshotHistory.map((snapshot, idx) => {
+        console.log('idx in SnapShotContainer: ', idx);
+        return <Snapshot key={idx.toString()} idx={idx} snapshot={snapshot} />;
+      })}
+      {/* <p>{JSON.stringify(snapshotHistory)}</p> */}
     </div>
   );
 }
