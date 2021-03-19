db5001c56b02b9369eba42a2fbc58cbd4f1510b1
Stanley Huang <iamstanleyhuang@gmail.com>
2021-03-18 19:53:02 -0700

    Add auto scroll to bottom when snapshot container becomes filled

--- src/app/containers/SnapShotContainer.tsx
+++ src/app/containers/SnapShotContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { snapshotHistoryContext } from '../components/App';
 import { prevSnapMock } from '../../app/mock/mockStateDiff';
 import Snapshot from '../components/Snapshot/Snapshot';
@@ -7,12 +7,19 @@ function SnapShotContainer(): JSX.Element {
   const { snapshotHistory, setSnapshotHistory } = useContext<any>(
     snapshotHistoryContext
   );
+  const snapshotEndRef = useRef<HTMLDivElement>(null);
 
   const handleNewData = () => {
     const copy = { ...prevSnapMock };
     copy.resetSquaresAtom = { ...copy.resetSquaresAtom };
     copy.resetSquaresAtom.contents = Math.floor(Math.random() * 10000);
     setSnapshotHistory((prevState: any) => [...prevState, copy]);
+  };
+
+  useEffect(() => scrollToBottom(), [snapshotHistory]);
+
+  const scrollToBottom = (): void => {
+    snapshotEndRef?.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
@@ -28,12 +35,15 @@ function SnapShotContainer(): JSX.Element {
         ATOMIC{' '}
       </p>
       <div>
-        <button onClick={handleNewData} style={{marginBottom:"15px"}}>ADD SnapShot</button>
+        <button onClick={handleNewData} style={{ marginBottom: '15px' }}>
+          ADD SnapShot
+        </button>
       </div>
 
       {snapshotHistory.map((snapshot: any, idx: number) => (
         <Snapshot key={idx} idx={idx} snapshot={snapshot} />
       ))}
+      <div ref={snapshotEndRef} />
     </div>
   );
 }
