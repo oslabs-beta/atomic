e904b860ee8fc2f44c25e8ed810500fd603d8e5f
Logan Thies <logan.thies@icloud.com>
2021-03-22 18:28:06 -0700

    NPM package initialization

---
+++ package/index.js
@@ -0,0 +1,12 @@
+//Connect to devtool
+//Export component
+//Export useAtom wrapper
+//Import fiber helpers
+
+console.log('Hello NPM');
+
+const sayHi = () => {
+  console.log('hi');
+};
+
+module.exports = { sayHi };
