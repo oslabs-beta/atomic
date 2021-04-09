082bf0d4ab54fd7211b16bf90e88bd31df4a781a
Stanley Huang <iamstanleyhuang@gmail.com>
2021-04-08 19:42:58 -0700

    Add types for snapshot, snapshot history, component tree history, etc.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,23 +1,48 @@
 //Snapshot State(Atom(s)) taken from Atomic dev tool
-export type snapshot = {
+export type Snapshot = {
+  [key: string]: snapshotValue;
+};
+
+//Individual snapshot object
+export type SnapshotValue = {
   contents: any;
   readDependencies: string[];
   dependents: string[];
   components: string[];
 };
 
+//Snapshot history for context API
+export type SnapshotHistoryContext = {
+  snapshotHistory: Snapshot[] | [];
+  setSnapshotHistory?: React.Dispatch<React.SetStateAction<Snapshot[]>>;
+};
+
+//Snapshot index for context API
+export type SnapshotIndexContext = {
+  snapshotIndex: number;
+  setSnapshotIndex?: React.Dispatch<React.SetStateAction<number>>;
+};
+
 //Component Tree taken from Atomic dev tool
-export type componentTree = {
+export type ComponentTree = {
   children: object[];
   name: string;
   tag: number;
   atom: string[];
-  state: {};
-  props: {};
+  state: { [key: string]: any };
+  props: Record<string, unknown>;
+};
+
+//Component tree history for context API
+export type ComponentTreeHistoryContext = {
+  componentTreeHistory: ComponentTree[] | [];
+  setComponentTreeHistory?: React.Dispatch<
+    React.SetStateAction<ComponentTree[]>
+  >;
 };
 
 //Atom Network features: atom-to-component, atom-to-dependent, atom-to- readDependencies
-//Component Graph
+//Component Graph feature:
 export type LinkTypesProps = {
   width: number;
   height: number;
